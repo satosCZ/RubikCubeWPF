826a888634c3474df0b2e3765ca6f1986a8f91e4
satosCZ <jk.satos@gmail.com>
2020-09-17 21:13:59 +0200

    Create Faces.cs

---
+++ Enums/Faces.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3dWPFTutorial.Enums
+{
+    public enum Faces
+    {
+        F,  //Front  - Front Face
+        R,  //Right  - Right Face
+        U,  //Upper  - Top Face
+        L,  //Left   - Left Face
+        B,  //Back   - Back Face
+        D,  //Down   - Bottom Face
+    }
+
+    public enum FacesColor
+    {
+        White,
+        Yellow,
+        Orange,
+        Red,
+        Green,
+        Blue
+    }
+}
