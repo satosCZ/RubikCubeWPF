e95f5fa48ef7a7786a796c358d771c9adab3aa94
satosCZ <jk.satos@gmail.com>
2020-09-17 21:14:16 +0200

    Create Rotation.cs

---
+++ Enums/Rotation.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3dWPFTutorial.Enums
+{
+    //F,R,U,L,B,D = Rotation clockwise
+    //Fb,Rb,Ub,Lb,Bb,Db = Rotation counter-clockwise
+    //F2,R2,U2,L2,B2,D2 = Rotation clockwise twice
+
+    public enum WholeCubeRotation
+    {
+        F,  
+        R,
+        U,
+        L,
+        B,
+        D,
+        Fb,
+        Rb,
+        Ub,
+        Lb,
+        Bb,
+        Db,
+        F2,
+        R2,
+        U2,
+        L2,
+        B2,
+        D2
+    }
+
+    public enum FacesRotation
+    {
+        F,
+        R,
+        U,
+        L,
+        B,
+        D,
+        Fb,
+        Rb,
+        Ub,
+        Lb,
+        Bb,
+        Db,
+        F2,
+        R2,
+        U2,
+        L2,
+        B2,
+        D2
+    }
+}
